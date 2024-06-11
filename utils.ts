b9b03fdc37f7179e15e4a8ff1145a70aefefd1cf
VinoliaTsitsi <156648216+VinoliaTsitsi@users.noreply.github.com>
2024-06-11 05:32:33 +0200

    Update utils.ts

--- utils.ts
+++ utils.ts
@@ -1,47 +1,52 @@
 
-import { Review } from './interfaces'
-const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement
-const returningUserDisplay = document.querySelector('#returning-user') as HTMLElement
-const userNameDisplay = document.querySelector('#user') as HTMLElement
+const reviewTotalDisplay = document.querySelector("#reviews");
+const returningUserDisplay = document.querySelector("#returning-user");
+const userNameDisplay = document.querySelector("#user");
+import { LoyaltyUser, Permissions } from "./enums";
+import { Review } from "./interfaces";
 
-enum Permissions {
-    ADMIN = 'ADMIN', 
-    READ_ONLY = 'READ_ONLY'
+export function showReviewTotal(
+ value: number,
+ reviewer: string,
+ isLoyalty: LoyaltyUser
+) {
+ const iconDisplay = LoyaltyUser.GOLD_USER ? "⭐" : "";
+ reviewTotalDisplay.innerHTML =
+  value.toString() +
+  " review" +
+  makeMultiple(value) +
+  " | last reviewed by " +
+  reviewer +
+  " " +
+  iconDisplay;
 }
 
-enum LoyaltyUser {
-    GOLD_USER = 'GOLD_USER',
-    SILVER_USER = 'SILVER_USER',
-    BRONZE_USER = 'BRONZE_USER'
+export function populateUser(isReturning: boolean, userName: string) {
+ if (isReturning) {
+  returningUserDisplay.innerHTML = "back";
+ }
+ userNameDisplay.innerHTML = userName;
 }
 
-export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) {
-    const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
-    reviewTotalDisplay.innerHTML = value.toString() + ' review' + makeMultiple(value) + ' | last reviewed by ' + reviewer + ' ' + iconDisplay    
+export function showDetails(
+ value: boolean | Permissions,
+ element: HTMLDivElement,
+ price: number
+) {
+ if (value) {
+  const priceDisplay = document.createElement("div");
+  priceDisplay.innerHTML = price.toString() + "/night";
+  element.appendChild(priceDisplay);
+ }
 }
 
-export function populateUser(isReturning : boolean, userName: string ) {
-    if (isReturning == true){
-        returningUserDisplay.innerHTML = 'back'
-    }
-    userNameDisplay.innerHTML = userName
+export function makeMultiple(value: number): string {
+ if (value > 1 || value == 0) {
+  return "s";
+ } else return "";
 }
 
-export function showDetails(value: boolean | Permissions, element : HTMLDivElement, price: number) {
-    if (value) {
-        const priceDisplay = document.createElement('div')
-        priceDisplay.innerHTML = price.toString() + '/night'
-        element.appendChild(priceDisplay)
-    }
+export function getTopTwoReviews(reviews: Review[]): Review[] {
+ const sortedReviews = reviews.sort((a, b) => b.stars - a.stars);
+ return sortedReviews.slice(0, 2);
 }
-
-export function makeMultiple(value: number) : string {
-    if (value > 1 || value == 0) {
-        return 's'
-    } else return ''
-}
-
-export function getTopTwoReviews(reviews : Review[]) : Review[]  {
-    const sortedReviews = reviews.sort((a, b) => b.stars - a.stars)
-    return sortedReviews.slice(0,2)
-   }
